8f00a58f13b8cfb287fa8ce50514c97f595d5f71
Usman Khan <uskhan8019@gmail.com>
2023-07-25 11:50:16 +0500

    migration files for job module

---
+++ database/migrations/2023_07_24_144912_add_created_by_to_jobs.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('jobs', function (Blueprint $table) {
+            $table->unsignedBigInteger('created_by')->nullable()->after('created_at');
+
+            $table->foreign('created_by')->references('UserID')->on('user')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('jobs', function (Blueprint $table) {
+            //
+        });
+    }
+};
---
+++ resources/views/job/show.blade.php
@@ -0,0 +1,170 @@
+@extends('template.tmp')
+@section('title', $pagetitle)
+@section('content')
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+<div class="main-content">
+    <div class="page-content">
+        <div class="container-fluid">
+            @if (session('error'))
+            <div class="alert alert-{{ Session::get('class') }} p-1" id="success-alert">        
+                {{ Session::get('error') }}  
+            </div>
+            @endif
+            @if (count($errors) > 0)
+            <div>
+                <div class="alert alert-danger p-2 border-1">
+                   <p class="font-weight-bold"> There were some problems with your input.</p>
+                    <ul>
+                        
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+            @endif            
+            <div class="row">
+                <div class="col-12">
+                    <div class="card card-hover">
+                        <div class="card-header">
+                            <h4 class="mb-0 text-dark">Job Details</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="row mt-4">
+                                <div class="col-12 col-sm-6 col-md-4">
+                                    <p class="fw-bold">Job Title</p>
+                                    <p> <span class="card-text">{{$job->name ?? 'N/A'}}</span></p>
+                                </div>
+                                <div class="col-12 col-sm-6 col-md-4">
+                                    <p class="fw-bold">Controller Type</p>
+                                    <p> <span class="card-text">{{$job->controller_type ?? 'N/A'}}</span></p>
+                                </div>
+                                <div class="col-12 col-sm-6 col-md-4">
+                                    <p class="fw-bold">No of Steps</p>
+                                    <p> <span class="card-text">{{$job->no_of_steps ?? 'N/A'}}</span></p>
+                                </div>
+                            </div>
+
+                            <div class="row mt-4">
+                                <div class="col-12 col-sm-6 col-md-4">
+                                    <p class="fw-bold">Overseed Governer Voltage</p>
+                                    <p> <span class="card-text">{{$job->overspeed_governer_voltage ?? 'N/A'}}</span></p>
+                                </div>
+                                <div class="col-12 col-sm-6 col-md-4">
+                                    <p class="fw-bold">Brake Voltage</p>
+                                    <p> <span class="card-text">{{$job->brake_voltage ?? 'N/A'}}</span></p>
+                                </div>
+                                <div class="col-12 col-sm-6 col-md-4">
+                                    <p class="fw-bold">Moter</p>
+                                    <p> <span class="card-text">{{$job->moter ?? 'N/A'}}</span></p>
+                                </div>
+                            </div>
+                            <div class="row mt-4">
+                                <div class="col-12 col-sm-6 col-md-4">
+                                    <p class="fw-bold">Encode Type</p>
+                                    <p> <span class="card-text">{{$job->encoder_type ?? 'N/A'}}</span></p>
+                                </div>
+                                <div class="col-12 col-sm-6 col-md-4">
+                                    <p class="fw-bold">No of entrance</p>
+                                    <p> <span class="card-text">{{$job->no_of_entrance ?? 'N/A'}}</span></p>
+                                </div>
+                                <div class="col-12 col-sm-6 col-md-4">
+                                    <p class="fw-bold">Resue</p>
+                                    <p> <span class="card-text">{{$job->resue ?? 'N/A'}}</span></p>
+                                </div>
+                            </div>
+                            <div class="row mt-4">
+                                <div class="col-12 col-sm-6 col-md-4">
+                                    <p class="fw-bold">Delivery Date</p>
+                                    <p> <span class="card-text">{{$job->delivery_date ?? 'N/A'}}</span></p>
+                                </div>
+                                <div class="col-12 col-sm-6 col-md-4">
+                                    <p class="fw-bold">Door Type</p>
+                                    <p> <span class="card-text">{{$job->door_type ?? 'N/A'}}</span></p>
+                                </div>
+                                <div class="col-12 col-sm-6 col-md-4">
+                                    <p class="fw-bold">Other Materials</p>
+                                        @if($job->other_materials)
+                                        <p>
+                                        @foreach($job->other_materials as $material)
+                                            <span class="card-text">{{$material}}</span>,
+                                        @endforeach
+                                        </p>
+                                        @endif
+                                </div>
+                            </div>
+                            <div class="row mt-4">
+                                <div class="col-12 col-sm-8 col-md-6">
+                                    <p class="fw-bold">Additional Details</p>
+                                    <p> <span class="card-text">{{$job->additional_details ?? 'N/A'}}</span></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @if(session::get('UserType') == 'User')
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card card-hover">
+                            <div class="card-header">
+                                <h4 class="mb-0 text-dark">Reply</h4>
+                            </div>
+                            <div class="card-body">
+                                <form method="post" action="{{route('job.updateStatus')}}">
+                                    {{csrf_field()}}
+                                    <input type="hidden" name="job_id" value="{{$job->id}}">
+                                    <div class="row mt-4">
+                                        <div class="col-md-6">
+                                            <div class="mb-3">
+                                                <label for="basicpill-firstname-input">Reply<span class="text-danger">*</span></label>
+                                                <textarea name="reply" class="form-control" rows="15" required>
+                                                    {{@$userJob->reply}}
+                                                </textarea>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="mb-3">
+                                                <label for="col-form-label">Status<span class="text-danger">*</span></label><br>
+                                                <select name="job_status" id="status" class="form-select select2" required="" style="width:90%">
+                                                    <option value="pending" {{@$userJob->status == 'pending' ? 'selected': ''}}>Pending</option>
+                                                    <option value="in-progress" {{@$userJob->status == 'in-progress' ? 'selected': ''}}>In Progress</option>
+                                                    <option value="completed" {{@$userJob->status == 'completed' ? 'selected': ''}}>Completed</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row mt-4">
+                                        <div class="col-lg-8 col-12">
+                                            <div class="mt-2"><button type="submit" class="btn btn-success w-md float-right" >Update Status</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endif
+        </div>
+    </div>
+</div>
+<script type="text/javascript">
+$(document).ready(function() {
+
+    function delete_confirm2(url,id) {        
+
+
+        url = '{{URL::TO('/')}}/'+url+'/'+ id;
+        
+    
+       
+        jQuery('#staticBackdrop').modal('show', {backdrop: 'static'});
+        document.getElementById('delete_link').setAttribute('href' , url);
+         
+    }
+});
+</script>
+@endsection
